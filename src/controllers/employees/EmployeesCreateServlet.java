06ba277a8e779d81c3f70f76034ef2fed2ff07dc
kaykay <kdotjelly@gmail.com>
2020-08-05 10:15:35 +0900

    Add Employee create

---
+++ src/controllers/employees/EmployeesCreateServlet.java
@@ -0,0 +1,91 @@
+package controllers.employees;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Employee;
+import models.validators.EmployeeValidator;
+import utils.DBUtil;
+import utils.EncryptUtil;
+
+/**
+ * Servlet implementation class EmployeesCreateServlet
+ */
+@WebServlet("/employees/create")
+public class EmployeesCreateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmployeesCreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = (String)request.getParameter("_token");
+        //<- new <- newservlet(request.setAttribute("_token", request.getSession().getId())
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
+
+            Employee e = new Employee();
+
+            e.setCode(request.getParameter("code"));
+            e.setName(request.getParameter("name"));
+            e.setPassword(
+                    EncryptUtil.getPasswordEncrypt(
+                             request.getParameter("password"),
+                             (String)this.getServletContext().getAttribute("salt")
+                             // why(String)? getAttributeで取った値は確かオブジェクト型に変換されてしまうから?
+                            )
+                    );
+            e.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
+            //why parse?
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+           e.setCreated_at(currentTime);
+           e.setUpdated_at(currentTime);
+           e.setDelete_flag(0);
+
+           List<String> errors = EmployeeValidator.validate(e, true, true);
+           //このクラスの処理は新規登録だから
+           //第2、第３引数のcode_duplicate_check_flag, password_check_flagをtrueにしてチェック処理を行わせたい。
+           if(errors.size() > 0) {
+               em.close();
+
+               request.setAttribute("_token", request.getSession().getId());
+               //<- new <- newservlet(request.setAttribute("_token", request.getSession().getId())
+               request.setAttribute("employee", e);
+               request.setAttribute("errors", errors);
+
+               RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/employees/new.jsp");
+               rd.forward(request, response);
+           } else {
+               em.getTransaction().begin();
+               em.persist(e);
+               em.getTransaction().commit();
+               em.close();
+               request.getSession().setAttribute("flush", "登録が完了しました。");
+
+               response.sendRedirect(request.getContextPath() + "/employees/index");
+               //-> indexservletで準備 -> index.jspで表示
+               }
+
+        }
+
+
+    }
+
+}
